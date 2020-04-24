20c5ca29cb1d70c1dbf85eebb04108f4ca8152fa
Luis Angel <mzlo126108@upemor.edu.mx>
2020-04-23 19:47:57 -0500

    Redirect http to https ok

--- src/main/java/com/demo/config/RedirectHttpToHttps.java
+++ src/main/java/com/demo/config/RedirectHttpToHttps.java
@@ -1,37 +1,40 @@
 package com.demo.config;
 
-import javax.persistence.Embedded;
-import javax.sql.DataSource;
-
-import com.demo.service.UserDetailService;
+import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.EmbeddedValueResolverAware;
-import org.springframework.context.*;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
-import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
-//Disable this configuration for packagin without valid ssl certificate. Also check the changes in properties
 @Configuration
 public class RedirectHttpToHttps {
 
-    /*@Bean
-    public EmbeddedServletContainerFactory*/
+    @Bean
+    public ServletWebServerFactory servletContainer () {
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(redirectConnector());
+        return tomcat;
+    }
 
-//    private Connector initiateHttpConnector() {
-//        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-//        connector.setScheme("http");
-//        connector.setPort(8082);
-//        connector.setSecure(false);
-//        connector.setRedirectPort(8443);
-//
-//        return connector;
-//    }
+    private Connector redirectConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(8080);
+        connector.setSecure(false);
+        connector.setRedirectPort(8443);
+        return connector;
+    }
 }
